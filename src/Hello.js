9728c42258cf999773d6f15dca080291687c794b
Aleksander Keremidarov <a.keremidarov@vconnect.dk>
2020-12-01 14:16:22 +0200

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi Sasho</h1>
+                <p>React is awesome</p>
+            </div>
+
+        )
+    }
+}
+
+export default Hello
